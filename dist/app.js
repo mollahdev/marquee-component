07fb049d68101d833e4de16d47ddfdda45f23ccb
Ashraf Mollah <mollah.dev@gmail.com>
2023-12-01 12:32:03 +0600

    added tailwind support

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,16 @@
-var v = Object.defineProperty;
-var N = (n, e, i) => e in n ? v(n, e, { enumerable: !0, configurable: !0, writable: !0, value: i }) : n[e] = i;
-var l = (n, e, i) => (N(n, typeof e != "symbol" ? e + "" : e, i), i), M = (n, e, i) => {
-  if (!e.has(n))
+var S = Object.defineProperty;
+var M = (h, e, i) => e in h ? S(h, e, { enumerable: !0, configurable: !0, writable: !0, value: i }) : h[e] = i;
+var c = (h, e, i) => (M(h, typeof e != "symbol" ? e + "" : e, i), i), N = (h, e, i) => {
+  if (!e.has(h))
     throw TypeError("Cannot " + i);
 };
-var E = (n, e, i) => {
-  if (e.has(n))
+var E = (h, e, i) => {
+  if (e.has(h))
     throw TypeError("Cannot add the same private member more than once");
-  e instanceof WeakSet ? e.add(n) : e.set(n, i);
+  e instanceof WeakSet ? e.add(h) : e.set(h, i);
 };
-var c = (n, e, i) => (M(n, e, "access private method"), i);
-const H = "marquee-component", O = "marquee-move", I = "marquee-slide", A = "marquee-mount", t = Object.freeze({
+var p = (h, e, i) => (N(h, e, "access private method"), i);
+const $ = "marquee-component", g = "marquee-move", I = "marquee-slide", u = "marquee-mount", t = Object.freeze({
   HORIZONTAL: "horizontal",
   VERTICAL: "vertical"
 }), s = Object.freeze({
@@ -28,99 +28,102 @@ class a {
   }
   static getChildren(e) {
     const i = e.children;
-    return Array.from(i).filter((T) => T instanceof HTMLElement);
+    return Array.from(i).filter((f) => f instanceof HTMLElement);
   }
-  static getElementsSize(e, i, T) {
-    let h = 0;
-    return e.reduce((p, r) => {
-      if (!p) {
+  static getElementsSize(e, i, f) {
+    let o = 0;
+    return e.reduce((l, r) => {
+      if (!l) {
         switch (i) {
           case t.HORIZONTAL:
-            h = r.clientWidth;
+            o = r.clientWidth;
             break;
           case t.VERTICAL:
-            h = r.clientHeight;
+            o = r.clientHeight;
             break;
         }
         return r;
       }
-      const m = a.getElementOffset(p), o = a.getElementOffset(r);
-      switch (`${i}-${T}`) {
+      const m = a.getElementOffset(l), n = a.getElementOffset(r);
+      switch (`${i}-${f}`) {
         case `${t.HORIZONTAL}-${s.LTR}`:
-          h = r.clientWidth + h + (o.left - m.left - p.clientWidth);
+          o = r.clientWidth + o + (n.left - m.left - l.clientWidth);
           break;
         case `${t.HORIZONTAL}-${s.RTL}`:
-          h = r.clientWidth + h + (o.right - m.right - p.clientWidth);
+          o = r.clientWidth + o + (n.right - m.right - l.clientWidth);
           break;
         case `${t.VERTICAL}-${s.LTR}`:
-          h = r.clientHeight + h + (o.top - m.top - p.clientHeight);
+          o = r.clientHeight + o + (n.top - m.top - l.clientHeight);
           break;
         case `${t.VERTICAL}-${s.RTL}`:
-          h = r.clientHeight + h + (o.bottom - m.bottom - p.clientHeight);
+          o = r.clientHeight + o + (n.bottom - m.bottom - l.clientHeight);
           break;
       }
       return r;
-    }, null), h;
+    }, null), o;
   }
   static getScopeSize(e, i) {
     return i === t.HORIZONTAL ? e.offsetWidth : e.offsetHeight;
   }
-  static getVisibleElements(e, i, T, h) {
-    const p = [], r = [], m = (o, f) => {
-      o ? r.push(f) : p.push(f);
+  static getVisibleElements(e, i, f, o) {
+    const l = [], r = [], m = (n, d) => {
+      n ? r.push(d) : l.push(d);
     };
-    return e.forEach((o) => {
-      const f = a.getElementOffset(o);
-      switch (`${i}-${T}`) {
+    return e.forEach((n) => {
+      const d = a.getElementOffset(n);
+      switch (`${i}-${f}`) {
         case `${t.HORIZONTAL}-${s.LTR}`:
-          m(f.left + o.clientWidth <= h, o);
+          m(d.left + n.clientWidth <= o, n);
           break;
         case `${t.HORIZONTAL}-${s.RTL}`:
-          m(f.right + o.clientWidth <= h, o);
+          m(d.right + n.clientWidth <= o, n);
           break;
         case `${t.VERTICAL}-${s.LTR}`:
-          m(f.top + o.clientHeight <= h, o);
+          m(d.top + n.clientHeight <= o, n);
           break;
         case `${t.VERTICAL}-${s.RTL}`:
-          m(f.bottom + o.clientHeight <= 0, o);
+          m(d.bottom + n.clientHeight <= 0, n);
           break;
       }
     }), {
-      visibleElements: p,
+      visibleElements: l,
       inVisibleElements: r
     };
   }
 }
-var b, z, u, C, d, L, $, S, R, g, w;
-customElements.get(H) || customElements.define(H, (w = class extends HTMLElement {
+var O, v, A, z, T, L, b, C, R, H, w;
+customElements.get($) || customElements.define($, (w = class extends HTMLElement {
   constructor() {
     super(...arguments);
+    E(this, O);
+    E(this, A);
+    E(this, T);
     E(this, b);
-    E(this, u);
-    E(this, d);
-    E(this, $);
     E(this, R);
-    l(this, "speed", 5);
-    l(this, "mode", t.HORIZONTAL);
-    l(this, "direction", s.LTR);
-    l(this, "wrapper", document.createElement("div"));
-    l(this, "scopeSize", 0);
-    l(this, "wrapperSize", 0);
-    l(this, "moveTo", 0);
-    l(this, "point", 0);
+    c(this, "speed", 2);
+    // speed per frame
+    c(this, "mode", t.HORIZONTAL);
+    c(this, "direction", s.LTR);
+    c(this, "wrapper", document.createElement("div"));
+    c(this, "scopeSize", 0);
+    c(this, "wrapperSize", 0);
+    c(this, "moveTo", 0);
+    c(this, "point", 0);
+    c(this, "frameInterval", 1e3 / 60);
+    c(this, "previousTime", performance.now());
   }
   connectedCallback() {
-    this.speed = Number(this.getAttribute("speed")) || 5, this.mode = this.getAttribute("mode") || t.HORIZONTAL, this.direction = this.getAttribute("dir") || s.LTR, this.addEventListener(A, () => {
-      this.wrapper = this.querySelector(O), this.style.display = "block", this.style.overflow = "hidden", this.mode === "horizontal" ? (this.style.width = "100%", this.style.maxWidth = `${innerWidth}px`) : (this.style.height = this.style.height || "100vh", this.style.maxHeight = `${innerHeight}px`), this.scopeSize = a.getScopeSize(this, this.mode), this.wrapperSize = a.getElementsSize(a.getChildren(this.wrapper), this.mode, this.direction), c(this, b, z).call(this), c(this, u, C).call(this);
+    this.speed = Number(this.getAttribute("speed")) || 2, this.mode = this.getAttribute("mode") || t.HORIZONTAL, this.direction = this.getAttribute("dir") || s.LTR, this.addEventListener(u, () => {
+      this.wrapper = this.querySelector(g), this.style.display = "block", this.style.overflow = "hidden", this.mode === "horizontal" ? (this.style.width = "100%", this.style.maxWidth = `${innerWidth}px`) : (this.style.height = this.style.height || "100vh", this.style.maxHeight = `${innerHeight}px`), this.scopeSize = a.getScopeSize(this, this.mode), this.wrapperSize = a.getElementsSize(a.getChildren(this.wrapper), this.mode, this.direction), p(this, O, v).call(this), p(this, A, z).call(this);
     });
   }
-}, b = new WeakSet(), z = function() {
+}, O = new WeakSet(), v = function() {
   const e = this.wrapper.innerHTML;
   for (; this.wrapperSize < this.scopeSize + this.scopeSize / 3; )
     this.wrapper.innerHTML = this.wrapper.innerHTML + e, this.wrapperSize = a.getElementsSize(a.getChildren(this.wrapper), this.mode, this.direction);
-}, u = new WeakSet(), C = function() {
-  this.moveTo = this.wrapperSize - this.scopeSize, this.mode === t.VERTICAL && this.direction === s.RTL && (this.moveTo = this.scopeSize - this.wrapperSize, c(this, d, L).call(this, this.moveTo)), c(this, R, g).call(this);
-}, d = new WeakSet(), L = function(e) {
+}, A = new WeakSet(), z = function() {
+  this.moveTo = this.wrapperSize - this.scopeSize, this.mode === t.VERTICAL && this.direction === s.RTL && (this.moveTo = this.scopeSize - this.wrapperSize, p(this, T, L).call(this, this.moveTo)), requestAnimationFrame(p(this, R, H).bind(this));
+}, T = new WeakSet(), L = function(e) {
   switch (`${this.mode}-${this.direction}`) {
     case `${t.HORIZONTAL}-${s.LTR}`:
       this.wrapper.style.transform = `translateX(-${Math.ceil(e)}px)`;
@@ -135,36 +138,39 @@ customElements.get(H) || customElements.define(H, (w = class extends HTMLElement
       this.wrapper.style.transform = `translateY(${Math.ceil(e)}px)`;
       break;
   }
-}, $ = new WeakSet(), S = function() {
+}, b = new WeakSet(), C = function() {
   return this.mode === t.VERTICAL && this.direction === s.RTL ? this.moveTo + this.point >= 0 : this.point >= this.moveTo;
-}, R = new WeakSet(), g = function() {
-  if (this.point = this.point + Number(this.speed), this.mode === t.VERTICAL && this.direction === s.RTL ? c(this, d, L).call(this, this.moveTo + this.point) : c(this, d, L).call(this, this.point), c(this, $, S).call(this)) {
-    const { visibleElements: e, inVisibleElements: i } = a.getVisibleElements(a.getChildren(this.wrapper), this.mode, this.direction, this.point), T = a.getElementsSize(e, this.mode, this.direction);
-    i.forEach((h) => {
-      this.wrapper.appendChild(h);
-    }), this.point = T - this.scopeSize, this.mode === t.VERTICAL && this.direction === s.RTL ? c(this, d, L).call(this, this.moveTo + this.point) : c(this, d, L).call(this, this.point), requestAnimationFrame(() => c(this, R, g).call(this));
-  } else
-    requestAnimationFrame(() => c(this, R, g).call(this));
+}, R = new WeakSet(), H = function(e) {
+  const f = (e - this.previousTime) / this.frameInterval, o = this.speed * f;
+  if (this.previousTime = e, this.point = this.point + o, this.mode === t.VERTICAL && this.direction === s.RTL ? p(this, T, L).call(this, this.moveTo + this.point) : p(this, T, L).call(this, this.point), p(this, b, C).call(this)) {
+    const { visibleElements: l, inVisibleElements: r } = a.getVisibleElements(a.getChildren(this.wrapper), this.mode, this.direction, this.point), m = a.getElementsSize(l, this.mode, this.direction);
+    r.forEach((d) => {
+      this.wrapper.appendChild(d);
+    });
+    const n = m - this.scopeSize;
+    this.point = n > 0 ? n : 0, this.mode === t.VERTICAL && this.direction === s.RTL ? p(this, T, L).call(this, this.moveTo + this.point) : p(this, T, L).call(this, this.point);
+  }
+  requestAnimationFrame(p(this, R, H).bind(this));
 }, w));
-customElements.get(O) || customElements.define(O, class extends HTMLElement {
+customElements.get(g) || customElements.define(g, class extends HTMLElement {
   constructor() {
     super();
-    l(this, "mode", t.HORIZONTAL);
-    l(this, "direction", s.LTR);
+    c(this, "mode", t.HORIZONTAL);
+    c(this, "direction", s.LTR);
     const e = this.closest("marquee-component");
     e && (this.mode = e.getAttribute("mode") || t.HORIZONTAL, this.direction = e.getAttribute("dir") || s.LTR, this.style.display = "flex", this.mode === t.HORIZONTAL ? this.style.flexDirection = "row" : this.style.flexDirection = "column", this.mode === t.VERTICAL && this.direction === s.RTL && (this.style.flexDirection = "column-reverse"));
   }
   // on mount
   connectedCallback() {
-    this.addEventListener(A, () => {
-      const e = this.closest(H);
-      e && e.dispatchEvent(new Event(A));
+    this.addEventListener(u, () => {
+      const e = this.closest($);
+      e && e.dispatchEvent(new Event(u));
     }, { once: !0 });
   }
 });
 customElements.get(I) || customElements.define(I, class extends HTMLElement {
   connectedCallback() {
-    const n = this.closest(O);
-    n && n.dispatchEvent(new Event(A));
+    const h = this.closest(g);
+    h && h.dispatchEvent(new Event(u));
   }
 });
